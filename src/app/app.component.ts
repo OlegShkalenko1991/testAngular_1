1fd0f0b6455f35f6fa412b81c8e912cdc9671ddb
OlegShkalenko1991 <nowhookah@gmail.com>
2021-06-17 00:08:54 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testAngular';
+
+  public arrButtonsState:boolean[][] =
+  [
+    [true, true, true, true],
+    [true, true, true, true],
+    [true, true, true, true],
+    [true, true, true, true]
+  ];
+
+  public arrButtonsColor:string[] = ['red','orange', 'yellow', 'green'];
+
+  toggleHandler(x:number, y:number) {
+    this.arrButtonsState[x][y]= !this.arrButtonsState[x][y];
+  }
+
+  setColor(row:number){
+    return this.arrButtonsColor[row];
+  }
 }
